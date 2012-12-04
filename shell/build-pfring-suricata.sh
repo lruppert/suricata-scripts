813a76429acf97e2b81dc255b3179368c5cdb50d
Lou Ruppert <himself@louruppert.com>
2012-12-04 19:33:57 +0000

    Build it using pf_ring...
    	new file:   build-pfring-suricata.sh

---
+++ shell/build-pfring-suricata.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#
+# This handles the suricata-building stuff using pfring detailed on Martin
+# Holste's web site.
+#
+# If needed, we get the proxy squared away up front:
+#
+#http_proxy=
+#export http_proxy
+
+# We choose what version.
+SURICATAVERSION=1.4rc1
+#
+
+#
+# Now we take care of suricata
+#
+#
+cd /usr/local/src
+wget http://www.openinfosecfoundation.org/download/suricata-${SURICATAVERSION}.tar.gz
+tar -xzf suricata-${SURICATAVERSION}.tar.gz
+cd suricata-${SURICATAVERSION}
+./configure --enable-pfring --enable-prelude --prefix=/usr/local/suricata/${SURICATAVERSION} --with-libpfring-includes=/usr/local/pfring/include --with-libpfring-libraries=/usr/local/pfring/lib --with-libpcap-includes=/usr/local/pfring/include/pcap --with-libpcap-libraries=/usr/local/pfring/lib
+make -j 16 && make install
+mkdir -p /etc/suricata/rules
+mkdir /var/log/suricata
+ln -s /usr/local/suricata/${SURICATAVERSION} /usr/local/suricata/current
+#
+# Customizations are still needed. We do those by hand. Don't forget!
+#
+echo Suricata still needs to be customized
