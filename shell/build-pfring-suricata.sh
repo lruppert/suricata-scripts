45dea2591ec303121bf8dc70e6019de5742231b6
Lou Ruppert <himself@louruppert.com>
2012-12-14 19:41:46 +0000

    Commented out http_proxy stubs so that places with env vars already set don't need to touch the script. Fixed logic to specifically look for symlinks.

--- shell/build-pfring-suricata.sh
+++ shell/build-pfring-suricata.sh
@@ -6,8 +6,8 @@
 #
 # If needed, we get the proxy squared away up front:
 #
-http_proxy=
-export http_proxy
+#http_proxy=
+#export http_proxy
 
 # We choose what version.
 SURICATAVERSION=1.4
@@ -31,7 +31,7 @@ if [ ! -d /var/log/suricata ]; then
 mkdir /var/log/suricata
 fi
 
-if [ ! -f /usr/local/suricata/current ]; then
+if [ ! -h /usr/local/suricata/current ]; then
 ln -s /usr/local/suricata/${SURICATAVERSION} /usr/local/suricata/current
 fi
 #
